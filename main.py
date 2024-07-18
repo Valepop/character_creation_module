fa191e1e004fdc19604c3bb43008f857a89feca9
Валерий Ляпин <valepop4747@gmail.com>
2024-07-18 17:22:51 +0300

    ВеVersion no OOP

--- main.py
+++ main.py
@@ -91,6 +91,7 @@ def choice_char_class() -> str:
 
 
 if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name = input('...назови себя: ')
